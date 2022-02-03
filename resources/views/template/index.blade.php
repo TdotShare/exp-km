9bac5ca5432b8649681171b2072dbba31e4f09dd
Jirayu Ch <52649379+TdotShare@users.noreply.github.com>
2022-02-03 23:00:12 +0700

    update header text
    update header text

--- resources/views/template/index.blade.php
+++ resources/views/template/index.blade.php
@@ -74,7 +74,7 @@
       <a href="#" class="brand-link">
         <img src="{{URL::asset("assets/logo/irdrmuti_cri.png")}}" alt="AdminLTE Logo"
           class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">ActivityUser IRD</span>
+        <span class="brand-text font-weight-light">EXPKM-RMUTI</span>
       </a>
 
       <!-- Sidebar -->
